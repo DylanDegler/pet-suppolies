11b8c46ab6790852300021357dae4dd63e2f34df
jford7 <jford@dev-10.com>
2021-12-21 12:29:46 -0500

    contact us initial

---
+++ pet-suppolies/src/ContactUs.js
@@ -0,0 +1,20 @@
+
+function ContactUs() {
+    return (
+    <>
+    <h1>Contact Us!</h1>
+    <pre>
+        Alexandria Holecek
+            Email: Alkaho31@github.com
+        Dylan Degler
+            Email: DylanDegler@github.com
+        Juan Ford
+            Email: jford7@github.com
+        Moira Crawshaw
+            Email: moiracrawshaw@github.com
+    </pre>
+    </>
+    );
+}
+
+export default ContactUs;
