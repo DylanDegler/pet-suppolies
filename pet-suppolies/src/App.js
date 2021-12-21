e4416f2e46108138b4677f4e967e864ebd51dd70
moiracrawshaw <craws22m@mtholyoke.edu>
2021-12-21 11:33:06 -0600

    bootstrap and some styling in App

--- pet-suppolies/src/App.js
+++ pet-suppolies/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+//import "./App.css";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import AboutUs from "./AboutUs.js";
 import ContactUs from "./ContactUs.js";
@@ -6,27 +6,29 @@ import Products from "./Products.js";
 
 function App() {
   return (
-    <BrowserRouter>
-    <h1>Pet Suppolies</h1>
-    <Link to="/about"><button>About Us</button></Link>
-    <Link to="/contact"><button>Contact Us</button></Link>
-    <Link to="/products"><button>Products</button></Link>
+    <div className="App">
+      <BrowserRouter>
+      <h1 className="page-header">Pet Suppolies</h1>
+      <Link to="/about"><button className="btn btn-outline-info">About Us</button></Link>
+      <Link to="/contact"><button className="btn btn-outline-info">Contact Us</button></Link>
+      <Link to="/products"><button className="btn btn-outline-info">Products</button></Link>
 
-    <Switch>
-      <Route path = "/about">
-        <AboutUs/>
-      </Route>
+      <Switch>
+        <Route path = "/about">
+          <AboutUs/>
+        </Route>
 
-      <Route path = "/contact">
-        <ContactUs/>
-      </Route>
+        <Route path = "/contact">
+          <ContactUs/>
+        </Route>
 
-      <Route path = "/products">
-        <Products/>
-      </Route>
+        <Route path = "/products">
+          <Products/>
+        </Route>
 
-    </Switch>
-    </BrowserRouter>
+      </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
