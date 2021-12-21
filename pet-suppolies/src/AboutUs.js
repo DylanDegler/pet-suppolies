57d08d6f6cc540afcfa575ef4aa9c89999b387ca
Dylan Degler <deglerdylan@gmail.com>
2021-12-21 11:41:00 -0600

    dylan branch push

---
+++ pet-suppolies/src/AboutUs.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function AboutUs() {
+  return (
+    <div className="AboutUs">
+      <h1>About Us</h1>
+      <p>Basic paragraph about us that I am too lazy to write at this very moment.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
